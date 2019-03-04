474fde185e70002f1f24c9fc2905fcb2a23dd8a9
fabioCollini <fabio.collini@gmail.com>
2019-03-04 20:56:34 +0100

    Detail as dynamic feature module

--- app/src/main/java/it/codingjam/cleanweather/app/MainNavigatorImpl.kt
+++ app/src/main/java/it/codingjam/cleanweather/app/MainNavigatorImpl.kt
@@ -1,12 +1,12 @@
 package it.codingjam.cleanweather.app
 
 import android.app.Activity
+import android.content.ComponentName
 import android.content.Intent
-import it.codingjam.cleanweather.detail.DetailActivity
 import it.codingjam.cleanweather.main.MainNavigator
 
 class MainNavigatorImpl : MainNavigator {
     override fun openDetail(activity: Activity) {
-        activity.startActivity(Intent(activity, DetailActivity::class.java))
+        activity.startActivity(Intent().setComponent(ComponentName(activity.packageName, "it.codingjam.cleanweather.detail.DetailActivity")))
     }
 }
